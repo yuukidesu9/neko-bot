0fbecd6e711b88c05d48fdc043b8ebe49f7f8235
João Costa <joaoinstaller@gmail.com>
2018-10-30 19:48:39 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -10,17 +10,38 @@ async def on_ready():
      print("I'm on!")
 
 async def on_message(text):
-     hello_a = "Oh, hi!"
-     hello_b = "Hey there!"
-     hello_c = "Hello!"
-     hello_d = "Hey, what's up?"
-     hello_e = "Hewwo :3"
-     hello_f = "Um... hi?"
+      hello_a = "Oh, hi!"
+      hello_b = "Hey there!"
+      hello_c = "Hello!"
+      hello_d = "Hey, what's up?"
+      hello_e = "Hewwo :3"
+      hello_f = "Um... hi?"
+      hellotext = [
+         'Oh, hi!',
+         'Hey there! o/',
+         'Hello! ^-^',
+         'Hey, what\'s up?',
+         'Henllo owo',
+         'Um... hi? \'-.-',
+     ]
+     greet = [
+          'Hello',
+          'Hi',
+          'Hewwo',
+          'Henllo',
+          'Hey',
+          'Hullo',
+     ]
+     command = [
+          'Ping',
+          'Roll',
+          'Hug',
+     ]
      if(text.author.id == 'BOT ID'): return
      else:
-          if text.content.startswith("Hello") or text.content.startswith("hello") or text.content.startswith("hullo") or text.content.startswith("Hullo") or text.content.startswith("Hi") or text.content.startswith("hi"):
-               answer = random.choice([hello_a, hello_b, hello_c, hello_d, hello_e, hello_f])
-               await client.send_message(text.channel, answer)
+          messagetext = text.content
+          if messagetext in greet:
+               await client.send_message(random.choice(possible_responses))
                return
           if text.content.startswith("Ping") or text.content.startswith("ping"):
                await client.send_message(text.channel, "Pong, I guess?")
