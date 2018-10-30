26948c85ad6b241d612d641c636ff676638857ee
João Costa <joaoinstaller@gmail.com>
2018-10-30 19:59:47 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -10,12 +10,6 @@ async def on_ready():
      print("I'm on!")
 
 async def on_message(text):
-    hello_a = "Oh, hi!"
-    hello_b = "Hey there!"
-    hello_c = "Hello!"
-    hello_d = "Hey, what's up?"
-    hello_e = "Hewwo :3"
-    hello_f = "Um... hi?"
     hellotext = [
         'Oh, hi!',
         'Hey there! o/',
@@ -24,26 +18,18 @@ async def on_message(text):
         'Henllo owo',
         'Um... hi? \'-.-',
     ]
-    greet = [
-         'Hello',
-         'Hi',
-         'Hewwo',
-         'Henllo',
-         'Hey',
-         'Hullo',
-    ]
-    command = [
-         'Ping',
-         'Roll',
-         'Hug',
-    ]
+    pongs = [
+        'Pong! ^-^',
+        'Um... pong?',
+        'Am I supposed to tell \'pong\'...?',
+        'Pong, I... guess?'
     if(text.author.id == 'BOT ID'): return
     else:
          messagetext = text.content
-         if messagetext in greet:
-              await client.send_message(random.choice(possible_responses))
+         if messagetext.startswith("Hello") or messagetext.startswith("Hey"):
+              await client.send_message(random.choice(hellotext))
               return
-         if messagetext in command:
-              await client.send_message(text.channel, "Pong, I guess?")
+         if messagetext == "ping" or messagetext == "Ping":
+              await client.send_message(random.choice(pongs))
               return
 client.run('NTA2ODg1MzQ3OTk3Nzc3OTIy.DrpEog.fLjlzw02rXw5j68iEZuU7s0ZiI8')
