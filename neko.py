6cb3475965fae2597b1697d7c491b7fee5cf0e7f
YuukiDesu9 <joaoinstaller@gmail.com>
2018-11-09 15:54:19 -0300

    Updated AIML parsing code

--- neko.py
+++ neko.py
@@ -60,4 +60,4 @@ async def on_message(text):
       #if text.content.startswith("ping") or text.content.startswith("Ping"):
       #     await client.send_message(text.channel, random.choice(pongs))
       #     return
-client.run('NTA2ODg1MzQ3OTk3Nzc3OTIy.DrpEog.fLjlzw02rXw5j68iEZuU7s0ZiI8')
+client.run('NTA5ODY4MzQwMzQ5NjMyNTIy.DsdpLw.CxJ4Sv1ysvOMEVdaE0LCmECVkdk')
